113edc32068cfaf1cf5a8eebaaae2a59d0c9c5af
adam adifam <adamabdi.al.a@gmail.com>
2025-01-02 18:38:50 +0700

    update

--- routes/web.php
+++ routes/web.php
@@ -215,9 +215,9 @@ Route::middleware(['auth:user', 'ceklevel:admin'])->group(function () {
 
 Route::middleware(['auth:guru', 'ceklevel:guru'])->group(function () {
     Route::get('/dashboardguru', 'DashboardController@guru')->name('dashboardguru');
-    Route::get('/presensisiswa/{kode_jadwal}/create', 'PresensisiswaController@create');
-    Route::get('/presensisiswa/{kode_jadwal}/cetak', 'PresensisiswaController@cetak');
-    Route::post('/presensisiswa/{kode_jadwal}/store', 'PresensisiswaController@store')->name('presensisiswa.store');
+    // Route::get('/presensisiswa/{kode_jadwal}/create', 'PresensisiswaController@create');
+    // Route::get('/presensisiswa/{kode_jadwal}/cetak', 'PresensisiswaController@cetak');
+    // Route::post('/presensisiswa/{kode_jadwal}/store', 'PresensisiswaController@store')->name('presensisiswa.store');
 });
 
 Route::middleware(['auth:karyawan', 'ceklevel:user'])->group(function () {
